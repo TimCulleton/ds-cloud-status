6207dac83972ffc5fa6a237f1ba33ef5b688e4eb
Tim Culleton <mit_notelluc@hotmail.com>
2021-12-16 13:26:08 +1000

    Initial Project Setup
    Basic Dir structure + deps and Test scaffolds

---
+++ src/server/Test.ts
@@ -0,0 +1,8 @@
+export function doSomething(name: string): string {
+    return name;
+}
+
+export function doSomethingElse(): void {
+    // eslint-disable-next-line no-console
+    console.info('dd');
+}
